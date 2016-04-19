099f8c79a1eee8a1eb3963a7dfce28a1baa717bd
John P. Masseria <jpmasseria@gmail.com>
2016-04-19 18:23:44 -0400

    Clean up code in PiTFTWeather.py

--- PiTFTWeather.py
+++ PiTFTWeather.py
@@ -94,136 +94,134 @@ fontTime = pygame.font.Font(fontpath, 40)
 class MyDisplay:
     # implement run method
     def run(self):
-            # count minutes
-            count = 0
-            # compare when minute changes
-            # set to something which will initially never match so display updates 1st time through loop
-            tempMin = 60
-            # also get the weather the first time through
-            weather_com_result = pywapi.get_weather_from_weather_com(weatherDotComLocationCode,units = 'imperial')
-            while True:
-                if tempMin != datetime.now().strftime('%M'):
-                    # update tempMin
-                    tempMin = datetime.now().strftime('%M')
-                    # increment minute count
-                    count = count + 1
+        # count minutes
+        count = 0
+        # compare when minute changes
+        # set to something which will initially never match so display updates 1st time through loop
+        tempMin = 60
+        # also get the weather the first time through
+        weather_com_result = pywapi.get_weather_from_weather_com(weatherDotComLocationCode,units = 'imperial')
+        while True:
+            if tempMin != datetime.now().strftime('%M'):
+                # update tempMin
+                tempMin = datetime.now().strftime('%M')
+                # increment minute count
+                count = count + 1
 
-                    # update weather every 5 minutes
-                    if count == 5:
-                        # reset the count
-                        count = 0
-                        # retrieve data from weather.com
-                        weather_com_result = pywapi.get_weather_from_weather_com(weatherDotComLocationCode,units = 'imperial')
+                # update weather every 5 minutes
+                if count == 5:
+                    # reset the count
+                    count = 0
+                    # retrieve data from weather.com
+                    weather_com_result = pywapi.get_weather_from_weather_com(weatherDotComLocationCode,units = 'imperial')
 
-                    # extract current data for today
-                    try:
-                        today = weather_com_result['forecasts'][0]['day_of_week'][0:3] + " " \
-                            + weather_com_result['forecasts'][0]['date'][4:] + " " \
-                            + weather_com_result['forecasts'][0]['date'][:3]
-                    except ValueError:
+                # extract current data for today
+                try:
+                    today = weather_com_result['forecasts'][0]['day_of_week'][0:3] + " " \
+                        + weather_com_result['forecasts'][0]['date'][4:] + " " \
+                        + weather_com_result['forecasts'][0]['date'][:3]
+                except ValueError:
+                    break
+
+                windSpeed = weather_com_result['current_conditions']['wind']['speed']
+                if windSpeed == "calm":
+                    windSpeed = '0'
+                currWind = windSpeed + " mph " + weather_com_result['current_conditions']['wind']['text']
+                currTemp = weather_com_result['current_conditions']['temperature'] + u'\N{DEGREE SIGN}' + "F"
+                # currPress = weather_com_result['current_conditions']['barometer']['reading'][:-3] + " in"
+                currPress = weather_com_result['current_conditions']['barometer']['reading'] + " in"
+                uv = "UV {}".format(weather_com_result['current_conditions']['uv']['text'])
+                humid = "Hum {}%".format(weather_com_result['current_conditions']['humidity'])
+
+                # extract forecast data
+                forecastDays = {}
+                forecaseHighs = {}
+                forecaseLows = {}
+                forecastPrecips = {}
+                forecastWinds = {}
+
+                start = 0
+                try:
+                    test = float(weather_com_result['forecasts'][0]['day']['wind']['speed'])
+                except ValueError:
+                    start = 1
+
+                for i in range(start, 5):
+
+                    if not(weather_com_result['forecasts'][i]):
                         break
+                    forecastDays[i] = weather_com_result['forecasts'][i]['day_of_week'][0:3]
+                    forecaseHighs[i] = weather_com_result['forecasts'][i]['high'] + u'\N{DEGREE SIGN}' + "F"
+                    forecaseLows[i] = weather_com_result['forecasts'][i]['low'] + u'\N{DEGREE SIGN}' + "F"
+                    forecastPrecips[i] = weather_com_result['forecasts'][i]['day']['chance_precip'] + "%"
+                    forecastWinds[i] = "{:.0f}".format(int(weather_com_result['forecasts'][i]['day']['wind']['speed'])) + \
+                        weather_com_result['forecasts'][i]['day']['wind']['text']
 
-                    windSpeed = weather_com_result['current_conditions']['wind']['speed']
-                    if windSpeed == "calm":
-                        windSpeed = '0'
-                    currWind = windSpeed + " mph " + weather_com_result['current_conditions']['wind']['text']
-                    currTemp = weather_com_result['current_conditions']['temperature'] + u'\N{DEGREE SIGN}' + "F"
-                    # currPress = weather_com_result['current_conditions']['barometer']['reading'][:-3] + " in"
-                    currPress = weather_com_result['current_conditions']['barometer']['reading'] + " in"
-                    uv = "UV {}".format(weather_com_result['current_conditions']['uv']['text'])
-                    humid = "Hum {}%".format(weather_com_result['current_conditions']['humidity'])
+                # blank the screen
+                mytft.screen.fill(colourBlack)
 
-                    # extract forecast data
-                    forecastDays = {}
-                    forecaseHighs = {}
-                    forecaseLows = {}
-                    forecastPrecips = {}
-                    forecastWinds = {}
+                # Render the weather logo at 0,0
+                icon = installPath+ (weather_com_result['current_conditions']['icon']) + ".png"
+                logo = pygame.image.load(icon).convert()
+                mytft.screen.blit(logo, (0, 0))
 
-                    start = 0
-                    try:
-                        test = float(weather_com_result['forecasts'][0]['day']['wind']['speed'])
-                    except ValueError:
-                        start = 1
+                # set the anchor for the current weather data text
+                textAnchorX = 140
+                textAnchorY = 5
+                textYoffset = 40
 
-                    for i in range(start, 5):
+                # add current weather data text artifacts to the screen
+                text_surface = font.render(today, True, colourWhite)
+                mytft.screen.blit(text_surface, (textAnchorX, textAnchorY))
+                textAnchorY+=textYoffset
+                text_surface = font.render(currTemp, True, colourWhite)
+                mytft.screen.blit(text_surface, (textAnchorX, textAnchorY))
+                textAnchorY+=textYoffset
+                text_surface = font.render(currWind, True, colourWhite)
+                mytft.screen.blit(text_surface, (textAnchorX, textAnchorY))
+                textAnchorY+=textYoffset
+                text_surface = font.render(currPress, True, colourWhite)
+                mytft.screen.blit(text_surface, (textAnchorX, textAnchorY))
+                textAnchorY+=textYoffset
+                text_surface = font.render(uv, True, colourWhite)
+                mytft.screen.blit(text_surface, (textAnchorX, textAnchorY))
+                textAnchorY+=textYoffset
+                text_surface = font.render(humid, True, colourWhite)
+                mytft.screen.blit(text_surface, (textAnchorX, textAnchorY))
 
-                        if not(weather_com_result['forecasts'][i]):
-                            break
-                        forecastDays[i] = weather_com_result['forecasts'][i]['day_of_week'][0:3]
-                        forecaseHighs[i] = weather_com_result['forecasts'][i]['high'] + u'\N{DEGREE SIGN}' + "F"
-                        forecaseLows[i] = weather_com_result['forecasts'][i]['low'] + u'\N{DEGREE SIGN}' + "F"
-                        forecastPrecips[i] = weather_com_result['forecasts'][i]['day']['chance_precip'] + "%"
-                        forecastWinds[i] = "{:.0f}".format(int(weather_com_result['forecasts'][i]['day']['wind']['speed'])) + \
-                            weather_com_result['forecasts'][i]['day']['wind']['text']
+                # set X axis text anchor for the forecast text
+                textAnchorX = 0
+                textXoffset = 130
+                textYoffset = 30
 
-                    # blank the screen
-                    mytft.screen.fill(colourBlack)
-
-                    # Render the weather logo at 0,0
-                    icon = installPath+ (weather_com_result['current_conditions']['icon']) + ".png"
-                    logo = pygame.image.load(icon).convert()
-                    mytft.screen.blit(logo, (0, 0))
-
-                    # set the anchor for the current weather data text
-                    textAnchorX = 140
-                    textAnchorY = 5
-                    textYoffset = 40
-
-                    # add current weather data text artifacts to the screen
-                    text_surface = font.render(today, True, colourWhite)
+                # add each days forecast text
+                for i in forecastDays:
+                    textAnchorY = 260
+                    text_surface = fontSm.render(forecastDays[int(i)], True, colourWhite)
                     mytft.screen.blit(text_surface, (textAnchorX, textAnchorY))
                     textAnchorY+=textYoffset
-                    text_surface = font.render(currTemp, True, colourWhite)
+                    text_surface = fontSm.render(forecaseHighs[int(i)], True, colourWhite)
                     mytft.screen.blit(text_surface, (textAnchorX, textAnchorY))
                     textAnchorY+=textYoffset
-                    text_surface = font.render(currWind, True, colourWhite)
+                    text_surface = fontSm.render(forecaseLows[int(i)], True, colourWhite)
                     mytft.screen.blit(text_surface, (textAnchorX, textAnchorY))
                     textAnchorY+=textYoffset
-                    text_surface = font.render(currPress, True, colourWhite)
+                    text_surface = fontSm.render(forecastPrecips[int(i)], True, colourWhite)
                     mytft.screen.blit(text_surface, (textAnchorX, textAnchorY))
                     textAnchorY+=textYoffset
-                    text_surface = font.render(uv, True, colourWhite)
+                    text_surface = fontSm.render(forecastWinds[int(i)], True, colourWhite)
                     mytft.screen.blit(text_surface, (textAnchorX, textAnchorY))
-                    textAnchorY+=textYoffset
-                    text_surface = font.render(humid, True, colourWhite)
-                    mytft.screen.blit(text_surface, (textAnchorX, textAnchorY))
+                    textAnchorX+=textXoffset
 
-                    # set X axis text anchor for the forecast text
-                    textAnchorX = 0
-                    textXoffset = 130
-                    textYoffset = 30
+                # set x axis text anchor for the time
+                textAnchorX = 420
+                textAnchorY = 5
 
-
-                    # add each days forecast text
-                    for i in forecastDays:
-                        textAnchorY = 260
-                        text_surface = fontSm.render(forecastDays[int(i)], True, colourWhite)
-                        mytft.screen.blit(text_surface, (textAnchorX, textAnchorY))
-                        textAnchorY+=textYoffset
-                        text_surface = fontSm.render(forecaseHighs[int(i)], True, colourWhite)
-                        mytft.screen.blit(text_surface, (textAnchorX, textAnchorY))
-                        textAnchorY+=textYoffset
-                        text_surface = fontSm.render(forecaseLows[int(i)], True, colourWhite)
-                        mytft.screen.blit(text_surface, (textAnchorX, textAnchorY))
-                        textAnchorY+=textYoffset
-                        text_surface = fontSm.render(forecastPrecips[int(i)], True, colourWhite)
-                        mytft.screen.blit(text_surface, (textAnchorX, textAnchorY))
-                        textAnchorY+=textYoffset
-                        text_surface = fontSm.render(forecastWinds[int(i)], True, colourWhite)
-                        mytft.screen.blit(text_surface, (textAnchorX, textAnchorY))
-                        textAnchorX+=textXoffset
-
-                    # set x axis text anchor for the time
-                    textAnchorX = 420
-                    textAnchorY = 5
-
-                    # print the time
-                    text_surface = fontTime.render(datetime.now().strftime('%H:%M'), True, colourWhite)
-                    mytft.screen.blit(text_surface, (textAnchorX, textAnchorY))
-                    # refresh the screen with all the changes
-                    pygame.display.update()
-
+                # print the time
+                text_surface = fontTime.render(datetime.now().strftime('%H:%M'), True, colourWhite)
+                mytft.screen.blit(text_surface, (textAnchorX, textAnchorY))
+                # refresh the screen with all the changes
+                pygame.display.update()
 
 if __name__ == "__main__":
     myDisplay = MyDisplay()
