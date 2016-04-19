2eae0a054b642365a4c792a5a2a5c4913d78a9ad
John P. Masseria <jpmasseria@gmail.com>
2016-04-19 13:27:01 -0400

    Run as a normal app instead of daemon for x11 usage

--- PiTFTWeather.py
+++ PiTFTWeather.py
@@ -9,7 +9,7 @@ from datetime import datetime
 import pywapi
 import string
 
-from daemon import Daemon
+#from daemon import Daemon
 
 # Weather Icons used with the following permissions:
 #
@@ -18,7 +18,7 @@ from daemon import Daemon
 #
 # The icons are free to use for Non-Commercial use, but If you use want to use it with your art please credit me and put a link leading back to the icons DA page - http://vclouds.deviantart.com/gallery/#/d2ynulp
 #
-# *** Not to be used for commercial use without permission! 
+# *** Not to be used for commercial use without permission!
 # if you want to buy the icons for commercial use please send me a note - http://vclouds.deviantart.com/ ***
 
 installPath = "/opt/PiTFTWeather/"
@@ -38,7 +38,7 @@ updateRate = 600 # seconds
 class pitft :
     screen = None;
     colourBlack = (0, 0, 0)
-    
+
     def __init__(self):
         "Ininitializes a new pygame screen using the framebuffer"
         # Based on "Python GUI in Linux frame buffer"
@@ -46,9 +46,11 @@ class pitft :
         disp_no = os.getenv("DISPLAY")
         if disp_no:
             print "I'm running under X display = {0}".format(disp_no)
+            driver = 'x11'
+        else:
+            os.putenv('SDL_FBDEV', '/dev/fb0')
+            driver = 'fbcon'
 
-        os.putenv('SDL_FBDEV', '/dev/fb0')
-        
         # Select frame buffer driver
         # Make sure that SDL_VIDEODRIVER is set
         driver = 'fbcon'
@@ -59,11 +61,14 @@ class pitft :
         except pygame.error:
             print 'Driver: {0} failed.'.format(driver)
             exit(0)
-        
-        size = (pygame.display.Info().current_w, pygame.display.Info().current_h)
-        self.screen = pygame.display.set_mode(size, pygame.FULLSCREEN)
+
+        if disp_no:
+            self.screen = pygame.display.set_mode((800,600))
+        else:
+            size = (pygame.display.Info().current_w, pygame.display.Info().current_h)
+            self.screen = pygame.display.set_mode(size, pygame.FULLSCREEN)
         # Clear the screen to start
-        self.screen.fill((0, 0, 0))        
+        self.screen.fill((0, 0, 0))
         # Initialise font support
         pygame.font.init()
         # Render the screen
@@ -85,8 +90,7 @@ font = pygame.font.Font(fontpath, 40)
 fontSm = pygame.font.Font(fontpath, 30)
 fontTime = pygame.font.Font(fontpath, 40)
 
-# Inherit from daemon class
-class Mydaemon(Daemon):
+class MyDisplay:
     # implement run method
     def run(self):
             # count minutes
@@ -97,7 +101,7 @@ class Mydaemon(Daemon):
             # also get the weather the first time through
             weather_com_result = pywapi.get_weather_from_weather_com(weatherDotComLocationCode,units = 'imperial')
             while True:
-                if tempMin != datetime.now().strftime('%M'): 
+                if tempMin != datetime.now().strftime('%M'):
                     # update tempMin
                     tempMin = datetime.now().strftime('%M')
                     # increment minute count
@@ -109,7 +113,7 @@ class Mydaemon(Daemon):
                         count = 0
                         # retrieve data from weather.com
                         weather_com_result = pywapi.get_weather_from_weather_com(weatherDotComLocationCode,units = 'imperial')
-                    
+
                     # extract current data for today
                     try:
                         today = weather_com_result['forecasts'][0]['day_of_week'][0:3] + " " \
@@ -127,22 +131,22 @@ class Mydaemon(Daemon):
                     currPress = weather_com_result['current_conditions']['barometer']['reading'] + " in"
                     uv = "UV {}".format(weather_com_result['current_conditions']['uv']['text'])
                     humid = "Hum {}%".format(weather_com_result['current_conditions']['humidity'])
-                    
+
                     # extract forecast data
                     forecastDays = {}
                     forecaseHighs = {}
                     forecaseLows = {}
                     forecastPrecips = {}
                     forecastWinds = {}
-                
+
                     start = 0
                     try:
                         test = float(weather_com_result['forecasts'][0]['day']['wind']['speed'])
                     except ValueError:
                         start = 1
-                
+
                     for i in range(start, 5):
-                    
+
                         if not(weather_com_result['forecasts'][i]):
                             break
                         forecastDays[i] = weather_com_result['forecasts'][i]['day_of_week'][0:3]
@@ -151,20 +155,20 @@ class Mydaemon(Daemon):
                         forecastPrecips[i] = weather_com_result['forecasts'][i]['day']['chance_precip'] + "%"
                         forecastWinds[i] = "{:.0f}".format(int(weather_com_result['forecasts'][i]['day']['wind']['speed'])) + \
                             weather_com_result['forecasts'][i]['day']['wind']['text']
-                        
+
                     # blank the screen
                     mytft.screen.fill(colourBlack)
-                    
+
                     # Render the weather logo at 0,0
                     icon = installPath+ (weather_com_result['current_conditions']['icon']) + ".png"
                     logo = pygame.image.load(icon).convert()
                     mytft.screen.blit(logo, (0, 0))
-                    
+
                     # set the anchor for the current weather data text
                     textAnchorX = 140
                     textAnchorY = 5
                     textYoffset = 40
-                    
+
                     # add current weather data text artifacts to the screen
                     text_surface = font.render(today, True, colourWhite)
                     mytft.screen.blit(text_surface, (textAnchorX, textAnchorY))
@@ -183,13 +187,13 @@ class Mydaemon(Daemon):
                     textAnchorY+=textYoffset
                     text_surface = font.render(humid, True, colourWhite)
                     mytft.screen.blit(text_surface, (textAnchorX, textAnchorY))
-                
+
                     # set X axis text anchor for the forecast text
                     textAnchorX = 0
                     textXoffset = 130
                     textYoffset = 30
 
-                    
+
                     # add each days forecast text
                     for i in forecastDays:
                         textAnchorY = 260
@@ -208,34 +212,18 @@ class Mydaemon(Daemon):
                         text_surface = fontSm.render(forecastWinds[int(i)], True, colourWhite)
                         mytft.screen.blit(text_surface, (textAnchorX, textAnchorY))
                         textAnchorX+=textXoffset
-    
+
                     # set x axis text anchor for the time
                     textAnchorX = 420
                     textAnchorY = 5
-                
+
                     # print the time
-                    text_surface = fontTime.render(datetime.now().strftime('%H:%M'), True, colourWhite) 
+                    text_surface = fontTime.render(datetime.now().strftime('%H:%M'), True, colourWhite)
                     mytft.screen.blit(text_surface, (textAnchorX, textAnchorY))
                     # refresh the screen with all the changes
                     pygame.display.update()
-                
-        
+
+
 if __name__ == "__main__":
-    daemon = Mydaemon('/tmp/PiTFTWeather.pid', stdout='/tmp/PiTFTWeather.log', stderr='/tmp/PiTFTWeatherErr.log')
-    if len(sys.argv) == 2:
-        if 'start' == sys.argv[1]:
-            syslog.syslog(syslog.LOG_INFO, "Starting")
-            daemon.start()
-        elif 'stop' == sys.argv[1]:
-            syslog.syslog(syslog.LOG_INFO, "Stopping")
-            daemon.stop()
-        elif 'restart' == sys.argv[1]:
-            syslog.syslog(syslog.LOG_INFO, "Restarting")
-            daemon.restart()
-        else:
-            print "Unknown command"
-            sys.exit(2)
-        sys.exit(0)
-    else:
-        print "usage: %s start|stop|restart" % sys.argv[0]
-        sys.exit(2)
+    myDisplay = MyDisplay()
+    myDisplay.run()
